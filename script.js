c9c0466588bcf71564a833cad8ddaaf220023380
Lucas Santos <43860524+LucasSansB@users.noreply.github.com>
2023-10-05 15:00:25 +0000

    feat(formulario)notas

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-console.log("Olá Mundo!");
-document.write("Salve Salve");
-console.log("Imprimindo no console");
-window.alert("Texto no modal do navegador");
+function mediaAluno() { 
+    console.log("Olá Mundo!");
+    console.log("Imprimindo no console");
+    window.alert("Texto no modal do navegador");
 
-//Lógica de programação
-//Problema: receber 3 notas de um aluno.
-//Calcular e apresentar a média e o resultado
-//se o aluno foi aprovado ou não
+    //Lógica de programação
+    //Problema: receber 3 notas de um aluno.
+    //Calcular e apresentar a média e o resultado
+    //se o aluno foi aprovado, reprovado ou ficou de exame
 
-console.log("Início do programa");
+    console.log("Início do programa");
 
-// const nota1 = 8;
-// const nota2 = 7;
-// const nota3 = 9;
+    //declaração das "variaveis"
+    const nota1 = parseInt(document.getElementById('nota1').value);
+    const nota2 = parseInt(document.getElementById('nota2').value);
+    const nota3 = parseInt(document.getElementById('nota3').value);
+    
 
-nota1 = parseInt(prompt("Digite a sua 1° nota:"));
-nota2 = parseInt(prompt("Digite a sua 2° nota:"));
-nota3 = parseInt(prompt("Digite a sua 3° nota:"));
+    //Cálculo da média
+    media = (nota1 + nota2 + nota3) / 3;
 
-//Cálculo da média
-media = (nota1 + nota2 + nota3) / 3;
+    document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media + '<br>';
+    console.log("\n");
+    console.log("Média é:" , media);
+    console.log("\n");
 
-console.log("A 1° nota é:" + nota1);
-console.log("A 2° nota é:" + nota2);
-console.log("A 3° nota é:" + nota3);
-console.log("\n");
+    if (media >= 7){
+        document.getElementById('resultado').innerHTML += Image;
+    } else if (media >= 5){
+        document.getElementById('resultado').innerHTML += 'EXAME';
+    } else {
+        document.getElementById('resultado').innerHTML += 'REPROVADO';
+    }
 
-console.log("Média é:" , media);
-console.log("\n");
-
-console.log("Fim do processo.");
-
-// console.log("A média do aluno é:" );
-// console.log(media);
+    console.log("Fim do processo.");
+}
